3dee9cce7a1338d27990729c5ad899e68aef1b3e
root <rohitbhakaregit>
2020-03-04 19:30:32 +0530

    integrated the slack and system monitoring code

--- monitor_system.py
+++ monitor_system.py
@@ -1,10 +1,12 @@
 import psutil
 import time
 import subprocess
-
 from os import system
 import platform
 from datetime import datetime
+import slack_integration   # imported slack integration code 
+
+
 
 def get_size(bytes, suffix="B"):
     """
@@ -22,11 +24,19 @@ def get_size(bytes, suffix="B"):
 
 def cpu_Monitor():
 	print("="*40, "CPU Monitoring", "="*40)
-	print("CPU Usage Per Core:")
+	print("CPU Usage: "+str(psutil.cpu_percent()))
+	if psutil.cpu_percent() > 80:
+		print("Reached CPU threashold ! sending notification..........")
+		slack_integration.post_message_to_slack("CPU reached maximum threashold","CPU_ALERT","https://cdn1.iconfinder.com/data/icons/tech-multimedia-2/64/CPU-hot_proc_PC_wet_burn-512.png")
+	
+	"""
+	Per CPU
 	for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
 		print(f"	Core {i}: {percentage}%")
 		print(f"	Total CPU Usage: {psutil.cpu_percent()}%")
-	
+		if psutil.cpu_percent() > 80:
+			slack_integration.post_message_to_slack("CPU reached maximum threashold","CPU_ALERT")
+	"""
 def memory_Monitor():
 	print("="*40, "Memory Information", "="*40)
 	# get the memory details
@@ -35,6 +45,9 @@ def memory_Monitor():
 	print(f"	Available: {get_size(svmem.available)}")
 	print(f"	Used: {get_size(svmem.used)}")
 	print(f"	Percentage: {svmem.percent}%")
+	if svmem.percent > 50:
+		print("Reached RAM threashold ! sending notification..........")
+		slack_integration.post_message_to_slack("RAM reached maximum threashold ","RAM_ALERT","https://www.freeiconspng.com/uploads/ram-icon-png-11.png")
 
 def disk_Monitor():
 	# Disk Information
@@ -44,6 +57,9 @@ def disk_Monitor():
 	print(f"	Used: {get_size(partition_usage.used)}")
 	print(f"  	Free: {get_size(partition_usage.free)}")
 	print(f"  	Percentage: {partition_usage.percent}%")
+	if partition_usage.percent > 90:
+		print("Reached DISK threashold ! sending notification..........")
+		slack_integration.post_message_to_slack("Disk reached maximum threashold ","Disk_ALERT","https://cdn1.iconfinder.com/data/icons/storage-data-6/24/storage_drive_disk_compact_alert_1-512.png")
 
 def network_Monitor(url):
 	print("="*40, "Network Latency for : "+ url + "="*40)
@@ -55,12 +71,11 @@ def network_Monitor(url):
 	print(f"	Max={network_lags[2]} ms")
 
 
-
-
-#while(1):
-cpu_Monitor()
-memory_Monitor()
-disk_Monitor()
-network_Monitor('talentica-all.com')
-#	time.sleep(2.4)
-#	system('clear')
+while(1):
+	cpu_Monitor()
+	memory_Monitor()
+	disk_Monitor()
+	network_Monitor('talentica-all.com')
+	
+	#slack_integration.post_message_to_slack("CPU reached maximum threashold Rohit_test")
+	
--- slack_integration.py
+++ slack_integration.py
@@ -1,19 +1,15 @@
-
 import requests
 import json
 
-def post_message_to_slack(text, blocks = None):
+def post_message_to_slack(text,uname,image_url,blocks = None): 
     return requests.post('https://slack.com/api/chat.postMessage', {
-        'token': "xoxp-979368346404-968354493955-979399085604-c416d442f729a7c9283c2d35d0c3440c",
+        'token': "xoxp-979368346404-968354493955-980955372132-343303a034502c9c9390c147d10dae75",
         'channel': "#mesh",
         'text': text,
-        'icon_url': "https://cdn1.iconfinder.com/data/icons/tech-multimedia-2/64/CPU-hot_proc_PC_wet_burn-512.png",
-        'username': 'CPU_BURN',
+        'icon_url': image_url,
+        'username': uname,
         'blocks': json.dumps(blocks) if blocks else None
     }).json()
-
-post_message_to_slack("CPU reached maximum threashold");
-
 """
 Slack URL : talentica-world.slack.com
 username bhakarerohit@gmail.com
