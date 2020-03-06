01aa47ece8168d47d34449259bcf519fc6478148
root <rohitbhakaregit>
2020-03-06 14:57:34 +0530

    changed slack_id

--- slack_integration.py
+++ slack_integration.py
@@ -3,7 +3,7 @@ import json
 
 def post_message_to_slack(text,uname,image_url,blocks = None): 
     return requests.post('https://slack.com/api/chat.postMessage', {
-        'token': "xoxp-979368346404-968354493955-985472461127-79d976c4f87692a6c38342e439c6df48",
+        'token': "xoxp-979368346404-968354493955-985214877941-72a30586dff2ee30179403386a30f11b",
         'channel': "#mesh",
         'text': text,
         'icon_url': image_url,
